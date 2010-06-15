c25857d149cae69c9d031a2dbfbf9bc892767552
dmitry <dmitry@ktulhu.(none)>
2010-06-15 22:44:54 +0400

    header

---
+++ src/view_kanji.h
@@ -0,0 +1,22 @@
+#ifndef VIEW_KANJI_INCLUDE
+
+#define VIEW_KANJI_INCLUDE
+
+enum
+{
+		NUMBER = 0,
+		KANJI,
+		KANJI_STROKE,
+		RADICAL,
+		RADICAL_STROKE,
+		ON,
+		KUN,
+		MEANING,
+		JLPT_LEVEL,
+		SCHOOL_GRADE,
+		COLUMNS
+};
+
+void view_kanji (GArray*);
+
+#endif
