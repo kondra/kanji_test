029d3884365a2773f57294442ad73e64e31c8353
kondra <dmitry@ktulhu.(none)>
2010-06-22 21:51:37 +0400

    ...

--- src/view_kanji.h
+++ src/view_kanji.h
@@ -2,18 +2,6 @@
 
 #define VIEW_KANJI_INCLUDE
 
-enum
-{
-		NUMBER = 0,
-		KANJI,
-		KANJI_STROKE,
-		RADICAL,
-		RADICAL_STROKE,
-		JLPT_LEVEL,
-		SCHOOL_GRADE,
-		COLUMNS
-};
-
 void view_kanji (GArray*);
 
 #endif
