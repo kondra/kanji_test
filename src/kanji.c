b5ee49e0e770148b50f0db32fd91bab35ad11ee8
kondra <dmitry@ktulhu.(none)>
2010-06-22 21:50:15 +0400

    new read/save, new Kanji structure

--- src/kanji.c
+++ src/kanji.c
@@ -1,17 +1,11 @@
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <gtk/gtk.h>
 
 #include "kanji.h"
-
-gboolean kanji_is_null (Kanji *k)
-{
-		if (k->radical_stroke || k->jlpt_level || k->grade || k->stroke || k->str != NULL || k->kun != NULL || k->on != NULL || k->meaning != NULL || k->radical != NULL)
-				return FALSE;
-		return TRUE;
-}
 
 Kanji* kanji_create_empty (void)
 {
@@ -20,25 +14,53 @@ Kanji* kanji_create_empty (void)
 		return k;
 }
 
-Kanji* kanji_create (const gchar *str, const gchar *radical, const gchar *kun, const gchar *on, const gchar *meaning, 
-				int jlpt_level, int grade, int stroke, int radical_stroke)
+Kanji* kanji_create (const gchar *str, const gchar *radical, const gchar *on, const gchar *meaning, 
+				int jlpt_level, int grade, int stroke, int radical_stroke, int num, gchar **writings, gchar **readings, gchar **meanings)
 {
+		int i;
+
 		Kanji *k = (Kanji*) g_malloc0 (sizeof (Kanji));
+
+		k->kun_writing = (gchar**) g_malloc0 (sizeof (gchar*) * num);
+		k->kun_reading = (gchar**) g_malloc0 (sizeof (gchar*) * num);
+		k->kun_meaning = (gchar**) g_malloc0 (sizeof (gchar*) * num);
+
+		for (i = 0; i < num; i++)
+		{
+				if (writings[i] == NULL)
+						k->kun_writing[i] = g_strdup (" ");
+				else
+						k->kun_writing[i] = g_strdup (writings[i]);
+
+				if (readings[i] == NULL)
+						k->kun_reading[i] = g_strdup (" ");
+				else
+						k->kun_reading[i] = g_strdup (readings[i]);
+
+				if (meanings[i] == NULL)
+						k->kun_meaning[i] = g_strdup (" ");
+				else
+						k->kun_meaning[i] = g_strdup (meanings[i]);
+		}
 
 		k->str = g_strdup_printf ("%s", str);
 		
 		k->radical = g_strdup_printf ("%s", radical);
 		
-		k->kun = g_strdup_printf ("%s", kun);
+//		k->kun = g_strdup_printf ("%s", kun);
 		
 		k->on = g_strdup_printf ("%s", on);
 		
-		k->meaning = g_strdup_printf ("%s", meaning);
+		if (meaning == NULL)
+				k->meaning = g_strdup_printf (" ");
+		else
+				k->meaning = g_strdup_printf ("%s", meaning);
 
 		k->jlpt_level = jlpt_level;
 		k->grade = grade;
 		k->stroke = stroke;
 		k->radical_stroke = radical_stroke;
+		k->num = num;
 
 		return k;
 }
@@ -54,7 +76,7 @@ GArray* kanji_array_load (const gchar *filename)
 		FILE *f;
 		GArray *arr = kanji_array_create;
 		gchar b, *buf;
-		int pos, len, curpos;
+		int pos, len, curpos, i;
 		struct stat statbuf;
 		unsigned int size;
 		Kanji *k;
@@ -86,14 +108,31 @@ GArray* kanji_array_load (const gchar *filename)
 				fread (&(k->grade), sizeof (int), 1, f);
 				fread (&(k->stroke), sizeof (int), 1, f);
 				fread (&(k->radical_stroke), sizeof (int), 1, f);
+				fread (&(k->num), sizeof (int), 1, f);
 				fread (&b, sizeof (gchar), 1, f);
 
 				//add more smart reading
 				curpos = ftell (f);
 				fread (buf, sizeof (gchar), 1000, f);
 
-				len = strlen (buf);
-				k->str = g_strdup_printf ("%s", buf);
+				k->kun_writing = (gchar**) g_malloc0 (sizeof (gchar*) * k->num);
+				k->kun_reading = (gchar**) g_malloc0 (sizeof (gchar*) * k->num);
+				k->kun_meaning = (gchar**) g_malloc0 (sizeof (gchar*) * k->num);
+
+				for (pos = 0, i = 0; i < k->num; i++)
+				{
+						k->kun_writing[i] = g_strdup(buf + pos);
+						pos += strlen (k->kun_writing[i]) + 1;
+
+						k->kun_reading[i] = g_strdup(buf + pos);
+						pos += strlen (k->kun_reading[i]) + 1;
+
+						k->kun_meaning[i] = g_strdup(buf + pos);
+						pos += strlen (k->kun_meaning[i]) + 1;
+				}
+
+				len = strlen (buf + pos);
+				k->str = g_strdup_printf ("%s", buf + pos);
 				if (k->str == NULL)
 				{
 						g_warning ("error parsing file %s", filename);
@@ -101,8 +140,8 @@ GArray* kanji_array_load (const gchar *filename)
 						arr = NULL;
 						break;
 				}
-				pos = len + 1;
-
+				pos += len + 1;
+/*
 				len = strlen (buf + pos);
 				k->kun = g_strdup_printf ("%s", buf + pos);
 				if (k->kun == NULL)
@@ -113,7 +152,7 @@ GArray* kanji_array_load (const gchar *filename)
 						break;
 				}
 				pos += len + 1;
-
+*/
 				len = strlen (buf + pos);
 				k->on = g_strdup_printf ("%s", buf + pos);
 				if (k->on == NULL)
@@ -162,7 +201,7 @@ GArray* kanji_array_load (const gchar *filename)
 void kanji_array_save (const gchar *filename, GArray *arr)
 {
 		FILE *f;
-		int i;
+		int i, j;
 		gchar b = 0;
 		Kanji *k;
 
@@ -170,26 +209,27 @@ void kanji_array_save (const gchar *filename, GArray *arr)
 				g_error ("can not open file");
 
 		k = &g_array_index (arr, Kanji, i = 0);
-		while (k == NULL || !kanji_is_null (k))
+		while (i < arr->len)
 		{
 				fwrite (&(k->jlpt_level), sizeof (int), 1, f);
 				fwrite (&(k->grade), sizeof (int), 1, f);
 				fwrite (&(k->stroke), sizeof (int), 1, f);
 				fwrite (&(k->radical_stroke), sizeof (int), 1, f);
+				fwrite (&(k->num), sizeof (int), 1, f);
 				fwrite (&b, sizeof (gchar), 1, f);
 
-				fwrite (k->str, sizeof (gchar), strlen (k->str), f);
-				fwrite (&b, sizeof (gchar), 1, f);
-				fwrite (k->kun, sizeof (gchar), strlen (k->kun), f);
-				fwrite (&b, sizeof (gchar), 1, f);
-				fwrite (k->on, sizeof (gchar), strlen (k->on), f);
-				fwrite (&b, sizeof (gchar), 1, f);
-				fwrite (k->meaning, sizeof (gchar), strlen (k->meaning), f);
-				fwrite (&b, sizeof (gchar), 1, f);
-				fwrite (k->radical, sizeof (gchar), strlen (k->radical), f);
-				fwrite (&b, sizeof (gchar), 1, f);
+				for (j = 0; j < k->num; j++)
+				{
+						fwrite (k->kun_writing[j], sizeof (gchar), strlen (k->kun_writing[j]) + 1, f);
+						fwrite (k->kun_reading[j], sizeof (gchar), strlen (k->kun_reading[j]) + 1, f);
+						fwrite (k->kun_meaning[j], sizeof (gchar), strlen (k->kun_meaning[j]) + 1, f);
+				}
 
-				//g_free (k);
+				fwrite (k->str, sizeof (gchar), strlen (k->str) + 1, f);
+				fwrite (k->on, sizeof (gchar), strlen (k->on) + 1, f);
+				fwrite (k->meaning, sizeof (gchar), strlen (k->meaning) + 1, f);
+				fwrite (k->radical, sizeof (gchar), strlen (k->radical) + 1, f);
+
 				k = &g_array_index (arr, Kanji, ++i);
 		}
 
