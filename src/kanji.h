03b20dc10fac7f032fa7b4d3df8de4f01356aec3
kondra <dmitry@ktulhu.(none)>
2010-06-22 21:50:37 +0400

    new Kanji structure

--- src/kanji.h
+++ src/kanji.h
@@ -4,18 +4,21 @@
 
 typedef struct
 {
+		int num;
+		gchar **kun_writing;
+		gchar **kun_reading;
+		gchar **kun_meaning;
+		
 		int jlpt_level, grade, stroke, radical_stroke;
 		gchar *str, *kun, *on, *meaning, *radical;
 } Kanji;
 
 #define kanji_array_create g_array_sized_new (TRUE, TRUE, sizeof (Kanji), 100)
 
-gboolean kanji_is_null (Kanji *k);
-
 Kanji* kanji_create_empty (void);
 
-Kanji* kanji_create (const gchar *str, const gchar *radical, const gchar *kun, const gchar *on, const gchar *meaning, 
-				int jlpt_level, int grade, int stroke, int radical_stroke);
+Kanji* kanji_create (const gchar *str, const gchar *radical, const gchar *on, const gchar *meaning, 
+				int jlpt_level, int grade, int stroke, int radical_stroke, int num, gchar **writings, gchar **readings, gchar **meanings);
 
 GArray* kanji_array_append (GArray *arr, Kanji *k);
 
