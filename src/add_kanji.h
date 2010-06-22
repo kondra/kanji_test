e4d347bd58c497b4e529e113c72fb9838a596f1c
kondra <dmitry@ktulhu.(none)>
2010-06-22 21:49:58 +0400

    Widgets removed

--- src/add_kanji.h
+++ src/add_kanji.h
@@ -2,13 +2,7 @@
 
 #define ADD_KANJI_INCLUDE
 
-typedef struct
-{
-		GtkWidget *dialog, *table1, *table2, *lbl, *expander;
-		GtkWidget *kanji_label, *on_label, *kun_label, *meaning_label, *jlpt_label, *grade_label, *radical_label, *stroke_label, *rst_label;
-		GtkWidget *kanji_entry, *on_entry, *kun_entry, *meaning_entry, *radical_entry, *jlpt_spin, *grade_spin, *stroke_spin, *rst_spin;
-} Widgets;
-
-Kanji* create_dialog (Kanji*, gboolean);
+Kanji* kanji_add_dialog (void);
+Kanji* kanji_edit_dialog (Kanji*);
 
 #endif
