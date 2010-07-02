651a414a3b7d36f8f68d0d2fa178cfbb51921d5c
dmitry <kondra2lp@gmail.com>
2010-07-02 04:06:10 +0400

    sorting + stroke

--- src/main2.c
+++ src/main2.c
@@ -20,6 +20,7 @@ typedef struct
 		gchar *kanji;
 		guint state;
 		
+		guint8 stroke;
 		guint8 num;
 		guint16 *radicals;
 } KanjiDecomposition;
@@ -29,13 +30,17 @@ typedef struct
 		gboolean cleared;
 		GArray *radicals;
 		GArray *kanji;
-		GtkWidget *label;
+		GArray *result;
+		GtkWidget *textview;
+		GtkTextBuffer *buffer;
 		GtkWidget *buttons[14][50];
+		GtkWidget *spin_min, *spin_max;
 } Widgets;
 
 static void destroy (GtkWidget*, gpointer);
 static void radical_button_toggled (GtkWidget*, Widgets*);
-static void clear (GtkWidget*, Widgets*);
+static void clear_button_pressed (GtkWidget*, Widgets*);
+static void stroke_range_changed (GtkWidget*, Widgets*);
 static GArray* radicals_process (const gchar*);
 static GArray* kanji_decomposition_process (const gchar*);
 
@@ -109,6 +114,7 @@ static GArray* kanji_decomposition_process (const gchar *filename)
 				g_array_index (arr, KanjiDecomposition, i).kanji = g_strdup (buf);
 				g_array_index (arr, KanjiDecomposition, i).state = 1;
 
+				fread (&(g_array_index (arr, KanjiDecomposition, i).stroke), sizeof (guint8), 1, f);
 				fread (&(g_array_index (arr, KanjiDecomposition, i).num), sizeof (guint8), 1, f);
 				
 				g_array_index (arr, KanjiDecomposition, i).radicals = (guint16*) g_malloc0 (sizeof (guint16) * g_array_index (arr, KanjiDecomposition, i).num);
@@ -119,21 +125,37 @@ static GArray* kanji_decomposition_process (const gchar *filename)
 		return arr;
 }
 
+gint compare (gconstpointer a, gconstpointer b, gpointer k)
+{
+		GArray *kanji = (GArray*) k;
+		if (g_array_index (kanji, KanjiDecomposition, *(guint16*)a).stroke < g_array_index (kanji, KanjiDecomposition, *(guint16*)b).stroke)
+				return -1;
+		if (g_array_index (kanji, KanjiDecomposition, *(guint16*)a).stroke > g_array_index (kanji, KanjiDecomposition, *(guint16*)b).stroke)
+				return 1;
+		return 0;
+}
+
 static void radical_button_toggled (GtkWidget *button, Widgets *p)
 {
 		GArray *radicals = p->radicals;
 		GArray *kanji = p->kanji;
+		GtkTextIter start, end;
+		GtkTextBuffer *buffer = p->buffer;
 
 		static guint count = 1;
 		guint save;
 
-		GtkLabel *label = GTK_LABEL (p->label);
 		guint k = GPOINTER_TO_UINT (g_object_get_data (G_OBJECT (button), "index"));
-		guint x, y;
+		guint x, y, cnt;
 		guint16 j, l, i;
 
-		gchar buf[10000];
+		GArray *result = p->result;
+		gchar buf[5000];
 		guint off = 0;
+		gint len = 0;
+
+		gint min = gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (p->spin_min));
+		gint max = gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (p->spin_max));
 
 		if (p->cleared)
 		{
@@ -148,8 +170,8 @@ static void radical_button_toggled (GtkWidget *button, Widgets *p)
 						l = g_array_index (radicals, Radical, k).kanji[i];
 						if (g_array_index (kanji, KanjiDecomposition, l).state == count)
 						{
-								sprintf (buf + off, "%s ", g_array_index (kanji, KanjiDecomposition, l).kanji);
-								off += strlen (g_array_index (kanji, KanjiDecomposition, l).kanji) + 1;
+								g_array_index (result, guint16, len) = l;
+								len++;
 
 								g_array_index (kanji, KanjiDecomposition, l).state ++;
 								for (j = 0; j < g_array_index (kanji, KanjiDecomposition, l).num; j++)
@@ -164,7 +186,29 @@ static void radical_button_toggled (GtkWidget *button, Widgets *p)
 						gtk_widget_set_sensitive (p->buttons[x][y], (g_array_index (radicals, Radical, i).state == count));
 				}
 
-				gtk_label_set_text (label, buf);
+				result->len = len;
+				g_array_sort_with_data (result, compare, (gpointer) kanji);
+				cnt = 0;
+				for (i = 0; i < len; i++)
+				{
+						l = g_array_index (result, guint16, i);
+						if (g_array_index (kanji, KanjiDecomposition, l).stroke > max || g_array_index (kanji, KanjiDecomposition, l).stroke < min)
+								continue;
+						sprintf (buf + off, "%s ", g_array_index (kanji, KanjiDecomposition, l).kanji);
+						off += strlen (g_array_index (kanji, KanjiDecomposition, l).kanji) + 1;
+						cnt++;
+						if (cnt >= 10)
+						{
+								buf[off++] = '\n';
+								cnt = 0;
+						}
+				}
+				buf[off] = 0;
+
+				gtk_text_buffer_get_start_iter (buffer, &start);
+				gtk_text_buffer_get_end_iter (buffer, &end);
+				gtk_text_buffer_delete (buffer, &start, &end);
+				gtk_text_buffer_insert_with_tags_by_name (buffer, &start, buf, -1, "kanji_font", NULL);
 				count++;
 		}
 		else
@@ -185,7 +229,10 @@ static void radical_button_toggled (GtkWidget *button, Widgets *p)
 						}
 						count--;
 						buf[0] = 0;
-						gtk_label_set_text (label, NULL); 
+						gtk_text_buffer_get_start_iter (buffer, &start);
+						gtk_text_buffer_get_end_iter (buffer, &end);
+						gtk_text_buffer_delete (buffer, &start, &end);
+						p->result->len = 0;
 						return;
 				}
 
@@ -199,6 +246,7 @@ static void radical_button_toggled (GtkWidget *button, Widgets *p)
 						if ( gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (p->buttons[x][y])) == FALSE)
 								continue;
 
+						cnt = 0;
 						for (i = 0; i < g_array_index (radicals, Radical, k).num; i++)
 						{
 								l = g_array_index (radicals, Radical, k).kanji[i];
@@ -206,8 +254,8 @@ static void radical_button_toggled (GtkWidget *button, Widgets *p)
 								{
 										if (count == save)
 										{
-												sprintf (buf + off, "%s ", g_array_index (kanji, KanjiDecomposition, l).kanji);
-												off += strlen (g_array_index (kanji, KanjiDecomposition, l).kanji) + 1;
+												g_array_index (result, guint16, l) = len;
+												len++;
 										}
 
 										g_array_index (kanji, KanjiDecomposition, l).state ++;
@@ -228,14 +276,38 @@ static void radical_button_toggled (GtkWidget *button, Widgets *p)
 				}
 				count++;
 
-				gtk_label_set_text (label, buf);
+				result->len = len;
+				g_array_sort_with_data (result, compare, (gpointer) kanji);
+				cnt = 0;
+				off = 0;
+				for (i = 0; i < len; i++)
+				{
+						l = g_array_index (result, guint16, i);
+						if (g_array_index (kanji, KanjiDecomposition, l).stroke > max || g_array_index (kanji, KanjiDecomposition, l).stroke < min)
+								continue;
+						sprintf (buf + off, "%s ", g_array_index (kanji, KanjiDecomposition, l).kanji);
+						off += strlen (g_array_index (kanji, KanjiDecomposition, l).kanji) + 1;
+						cnt++;
+						if (cnt >= 10)
+						{
+								buf[off++] = '\n';
+								cnt = 0;
+						}
+				}
+				buf[off] = 0;
+
+				gtk_text_buffer_get_start_iter (buffer, &start);
+				gtk_text_buffer_get_end_iter (buffer, &end);
+				gtk_text_buffer_delete (buffer, &start, &end);
+				gtk_text_buffer_insert_with_tags_by_name (buffer, &start, buf, -1, "kanji_font", NULL);
 		}
 }
 
-static void clear (GtkWidget *button, Widgets *p)
+static void clear_button_pressed (GtkWidget *button, Widgets *p)
 {
-		p->cleared = TRUE;
+		GtkTextIter start, end;
 		guint i, x, y;
+		p->cleared = TRUE;
 		for (i = 0; i < p->kanji->len; i++)
 				g_array_index (p->kanji, KanjiDecomposition, i).state = 1;
 
@@ -248,14 +320,75 @@ static void clear (GtkWidget *button, Widgets *p)
 				if (gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (p->buttons[x][y])) == TRUE)
 						gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (p->buttons[x][y]), FALSE);
 		}
-		gtk_label_set_text (GTK_LABEL (p->label), NULL); 
+		gtk_text_buffer_get_start_iter (p->buffer, &start);
+		gtk_text_buffer_get_end_iter (p->buffer, &end);
+		gtk_text_buffer_delete (p->buffer, &start, &end);
 }
 
+static void stroke_range_changed (GtkWidget *spin, Widgets *p)
+{
+		GtkTextIter start, end;
+
+		gint i, off, cnt;
+		gint min, max;
+		guint16 l;
+		gchar buf[5000];
+
+		if (spin == p->spin_min)
+		{
+				min = gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (p->spin_min));
+				max = gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (p->spin_max));
+				if (max < min)
+				{
+						gtk_spin_button_set_value (GTK_SPIN_BUTTON (p->spin_max), min);
+						return;
+				}
+		}
+		if (spin == p->spin_max)
+		{
+				min = gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (p->spin_min));
+				max = gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON (p->spin_max));
+				if (max < min)
+				{
+						gtk_spin_button_set_value (GTK_SPIN_BUTTON (p->spin_min), max);
+						return;
+				}
+		}
+
+		if (p->result->len == 0)
+				return;
+
+		cnt = off = 0;
+		buf[0] = 0;
+		for (i = 0; i < p->result->len; i++)
+		{
+				l = g_array_index (p->result, guint16, i);
+				if (g_array_index (p->kanji, KanjiDecomposition, l).stroke > max || g_array_index (p->kanji, KanjiDecomposition, l).stroke < min)
+						continue;
+				sprintf (buf + off, "%s ", g_array_index (p->kanji, KanjiDecomposition, l).kanji);
+				off += strlen (g_array_index (p->kanji, KanjiDecomposition, l).kanji) + 1;
+				cnt++;
+				if (cnt >= 10)
+				{
+						buf[off++] = '\n';
+						cnt = 0;
+				}
+		}
+		buf[off] = 0;
+
+		gtk_text_buffer_get_start_iter (p->buffer, &start);
+		gtk_text_buffer_get_end_iter (p->buffer, &end);
+		gtk_text_buffer_delete (p->buffer, &start, &end);
+		gtk_text_buffer_get_start_iter (p->buffer, &start);
+		gtk_text_buffer_insert_with_tags_by_name (p->buffer, &start, buf, -1, "kanji_font", NULL);
+}
 int main (int argc, char *argv[])
 {
 		GtkWidget *window;
-		GtkWidget *table, *scrolled;
-		GtkWidget *vbox, *hbox, *reset_button;
+		GtkWidget *table, *scrolled1, *scrolled2, *textview;
+		GtkWidget *vbox, *hbox1, *hbox2, *reset_button;
+		GtkWidget *label1, *label2;
+		GtkTextBuffer *buffer;
 
 		guint16 i, j;
 		guint k;
@@ -279,19 +412,28 @@ int main (int argc, char *argv[])
 
 		window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 		gtk_window_set_title (GTK_WINDOW (window), "Kanji");
-		gtk_widget_set_size_request (window, 450, 600);
+		gtk_widget_set_size_request (window, 900, 600);
 		gtk_container_set_border_width (GTK_CONTAINER (window), 10);
 
 		table = gtk_table_new (50, 14, FALSE);
 		gtk_table_set_row_spacings (GTK_TABLE (table), 2);
 		gtk_table_set_col_spacings (GTK_TABLE (table), 2);
 
-		p.label = gtk_label_new (NULL);
-		gtk_label_set_selectable (GTK_LABEL (p.label), TRUE);
+		//p.label = gtk_label_new (NULL);
+		//gtk_label_set_selectable (GTK_LABEL (p.label), TRUE);
 
+		textview = gtk_text_view_new ();
+		gtk_text_view_set_editable (GTK_TEXT_VIEW (textview), FALSE);
+		buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (textview));
+		gtk_text_buffer_create_tag (buffer, "kanji_font", "font", "20", NULL);
+
+		p.result = g_array_sized_new (TRUE, TRUE, sizeof (guint16), 2000);
+		p.result->len = 0;
 		p.cleared = FALSE;
 		p.radicals = radicals;
 		p.kanji = kanji;
+		p.buffer = buffer;
+		p.textview = textview;
 
 		for (k = 0, i = 0; i < 14; i++)
 		{
@@ -313,23 +455,44 @@ int main (int argc, char *argv[])
 				}
 		}
 
-		scrolled = gtk_scrolled_window_new (NULL, NULL);
-		gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-		gtk_container_set_border_width (GTK_CONTAINER (scrolled), 5);
-		gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scrolled), table);
+		scrolled1 = gtk_scrolled_window_new (NULL, NULL);
+		gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled1), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+		gtk_container_set_border_width (GTK_CONTAINER (scrolled1), 5);
+		gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scrolled1), table);
+
+		scrolled2 = gtk_scrolled_window_new (NULL, NULL);
+		gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled2), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+		gtk_container_set_border_width (GTK_CONTAINER (scrolled2), 5);
+		gtk_container_add (GTK_CONTAINER (scrolled2), textview);
 
 		g_signal_connect (G_OBJECT (window), "destroy", G_CALLBACK (destroy), NULL);
 
 		reset_button = gtk_button_new_from_stock (GTK_STOCK_CLEAR);
-		g_signal_connect (G_OBJECT (reset_button), "clicked", G_CALLBACK (clear), (gpointer) &p);
+		g_signal_connect (G_OBJECT (reset_button), "clicked", G_CALLBACK (clear_button_pressed), (gpointer) &p);
 
-		hbox = gtk_hbox_new (FALSE, 5);
-		gtk_box_pack_start (GTK_BOX (hbox), reset_button, FALSE, FALSE, 5);
+		label1 = gtk_label_new ("Stroke range:");
+		label2 = gtk_label_new ("-");
+		p.spin_min = gtk_spin_button_new_with_range (1, 30, 1);
+		g_signal_connect (G_OBJECT (p.spin_min), "value-changed", G_CALLBACK (stroke_range_changed), (gpointer) &p);
+		p.spin_max = gtk_spin_button_new_with_range (1, 30, 1);
+		gtk_spin_button_set_value (GTK_SPIN_BUTTON (p.spin_max), 30);
+		g_signal_connect (G_OBJECT (p.spin_max), "value-changed", G_CALLBACK (stroke_range_changed), (gpointer) &p);
+
+		hbox1 = gtk_hbox_new (FALSE, 5);
+		gtk_box_pack_start (GTK_BOX (hbox1), reset_button, FALSE, FALSE, 5);
+		gtk_box_pack_start (GTK_BOX (hbox1), label1, FALSE, FALSE, 5);
+		gtk_box_pack_start (GTK_BOX (hbox1), p.spin_min, FALSE, FALSE, 5);
+		gtk_box_pack_start (GTK_BOX (hbox1), label2, FALSE, FALSE, 5);
+		gtk_box_pack_start (GTK_BOX (hbox1), p.spin_max, FALSE, FALSE, 5);
+
+		hbox2 = gtk_hbox_new (FALSE, 5);
+		gtk_box_pack_start (GTK_BOX (hbox2), scrolled1, TRUE, TRUE, 5);
+		gtk_box_pack_start (GTK_BOX (hbox2), scrolled2, TRUE, TRUE, 5);
 
 		vbox = gtk_vbox_new (FALSE, 5);
-		gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 5);
-		gtk_box_pack_start (GTK_BOX (vbox), p.label, FALSE, FALSE, 5);
-		gtk_box_pack_start (GTK_BOX (vbox), scrolled, TRUE, TRUE, 5);
+		gtk_box_pack_start (GTK_BOX (vbox), hbox1, FALSE, FALSE, 5);
+//		gtk_box_pack_start (GTK_BOX (vbox), p.label, FALSE, FALSE, 5);
+		gtk_box_pack_start (GTK_BOX (vbox), hbox2, TRUE, TRUE, 5);
 
 		gtk_container_add (GTK_CONTAINER (window), vbox);
 		gtk_widget_show_all (window);
