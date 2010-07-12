aef74a63f51d6358801723bfa9706268c279473d
dmitry <kondra2lp@gmail.com>
2010-07-13 03:06:37 +0400

    new prototypes

--- src/kanji.h
+++ src/kanji.h
@@ -4,32 +4,39 @@
 
 typedef struct
 {
-		int num;
-		gchar **kun_writing;
-		gchar **kun_reading;
-		gchar **kun_meaning;
+		gint num;
+		gchar **word_writing;
+		gchar **word_reading;
+		gchar **word_meaning;
 
 		gboolean state;
 		
-		int jlpt_level, grade, stroke, radical_stroke;
-		gchar *str, *kun, *on, *meaning, *radical;
+		gint jlpt_level;
+		gint grade;
+		gint kanji_stroke;
+		gint radical_stroke;
+
+		gchar *kanji;
+		gchar *on;
+		gchar *meaning;
+		gchar *radical;
 } Kanji;
 
 #define kanji_array_create g_array_sized_new (TRUE, TRUE, sizeof (Kanji), 100)
 
 Kanji* kanji_create_empty (void);
 
-Kanji* kanji_create (const gchar *str, const gchar *radical, const gchar *on, const gchar *meaning, 
-				int jlpt_level, int grade, int stroke, int radical_stroke, int num, gchar **writings, gchar **readings, gchar **meanings);
+Kanji* kanji_create (const gchar*, const gchar*, const gchar*, const gchar*, 
+				gint, gint, gint, gint, gint, gchar**, gchar**, gchar**);
 
-GArray* kanji_array_append (GArray *arr, Kanji *k);
+GArray* kanji_array_append (GArray*, Kanji*);
 
-GArray* kanji_array_load (const gchar *filename);
+GArray* kanji_array_load (const gchar*);
 
-void kanji_array_save (const gchar *filename, GArray *arr);
+void kanji_array_save (const gchar*, GArray*);
 
-void kanji_array_free (GArray *arr);
+void kanji_array_free (GArray*);
 
-void kanji_free (Kanji *k);
+void kanji_free (Kanji*);
 
 #endif
