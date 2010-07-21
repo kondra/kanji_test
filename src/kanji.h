fd861621d708694d918c7baba4ae89cbf3fc2b09
dmitry <kondra2lp@gmail.com>
2010-07-21 16:03:11 +0400

    collocations and indexing support

--- src/kanji.h
+++ src/kanji.h
@@ -5,9 +5,21 @@
 typedef struct
 {
 		gint num;
+
+		gchar **writing;
+		gchar **reading;
+		gchar **meaning;
+		guint8 *level;
+} Collocations;
+
+typedef struct
+{
+		gint num;
 		gchar **word_writing;
 		gchar **word_reading;
 		gchar **word_meaning;
+
+		Collocations *col;
 
 		gboolean state;
 		
@@ -22,14 +34,21 @@ typedef struct
 		gchar *radical;
 } Kanji;
 
-#define kanji_array_create g_array_sized_new (TRUE, TRUE, sizeof (Kanji), 100)
+typedef struct
+{
+		gint ind;
+		gchar *word;
+} IndexEntry;
 
-Kanji* kanji_create_empty (void);
+//#define kanji_array_create g_array_sized_new (TRUE, TRUE, sizeof (Kanji), 100)
 
-Kanji* kanji_create (const gchar*, const gchar*, const gchar*, const gchar*, 
-				gint, gint, gint, gint, gint, gchar**, gchar**, gchar**);
+//Kanji* kanji_create_empty (void);
 
-GArray* kanji_array_append (GArray*, Kanji*);
+Collocations* collocations_create (gint, gchar**, gchar**, gchar**, guint8*, gboolean);
+
+Kanji* kanji_create (const gchar*, const gchar*, const gchar*, const gchar*, gint, gint, gint, gint, gint, gchar**, gchar**, gchar**);
+
+//GArray* kanji_array_append (GArray*, Kanji*);
 
 GArray* kanji_array_load (const gchar*);
 
@@ -38,5 +57,13 @@ void kanji_array_save (const gchar*, GArray*);
 void kanji_array_free (GArray*);
 
 void kanji_free (Kanji*);
+
+void kanji_index_save (const gchar*, GArray*);
+
+GArray* kanji_index_generate (GArray*);
+
+GArray* kanji_index_load (const gchar*);
+
+GArray* kanji_search (GArray*, const gchar*);
 
 #endif
